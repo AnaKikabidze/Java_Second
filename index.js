c5bfa8f6279d579261c94cfe2693532bade008af
AnaKikabidze <anastasiyagbm@gmail.com>
2023-03-24 22:20:20 +0500

    new_commit

--- index.js
+++ index.js
@@ -16,3 +16,21 @@ function message() {
   console.log("Я учу Java Script");
 }
 message();
+
+//Практическое задание:
+const blockFoto = document.getElementById("img");
+//console.log(blockFoto);
+function leftShowFoto() {
+  blockFoto.style.backgroundImage = `url("/assets/image/kitty_java_3.png")`;
+}
+function rightShowFoto() {
+  blockFoto.style.backgroundImage = `url("/assets/image/kitty_java_2.jpeg")`;
+}
+
+const blockFotoSecond = document.getElementById("big_cat");
+function leftShowFotoBig() {
+  blockFotoSecond.setAttribute("src", "assets/image/anime_cat2.jpeg");
+}
+function rightShowFotoBig() {
+  blockFotoSecond.setAttribute("src", "assets/image/cat_anime_3.jpeg");
+}
