376990127ef22ac0c5c491e6d5af99a42a0db48a
AnaKikabidze <anastasiyagbm@gmail.com>
2023-03-24 09:37:31 +0500

    second

---
+++ index.js
@@ -0,0 +1,18 @@
+// function showX(x) {
+//return x;
+//}
+//console.log(showX(28));
+
+//function sum(x, y = 0) {
+//  return x + y;
+//}
+//console.log(sum(5));
+
+//console.log(foo);
+//let foo = "Cat";
+//console.log(foo);
+
+function message() {
+  console.log("Я учу Java Script");
+}
+message();
